facd63f880a1b7796bb4515ab1cab07e3645c609
Doomtower <jessehudak707@outlook.com>
2023-01-21 00:58:31 -0800

    api key added for weather app

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+var APIKey = "34076cf95082839460dfb8d261ada036";
+
 const currentDay = document.querySelector(".currentDay");
 setInterval(() => {
   let time = dayjs().format("MM-DD-YYYY hh:mm:ss A");
