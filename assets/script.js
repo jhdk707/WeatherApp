def2f7f909d2b3b8a71b9c2314d278aa8a149744
Doomtower <jessehudak707@outlook.com>
2023-01-22 12:44:52 -0800

    created input form for city search

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,20 @@
-var APIKey = "34076cf95082839460dfb8d261ada036";
-var city; 
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key} - key for 5 day forcast
 
-// Timer function to present local date and time in header 
-const currentDay = document.querySelector(".currentDay");
-setInterval(() => {
-  let time = dayjs().format("MM-DD-YYYY hh:mm:ss A");
-  currentDay.innerHTML = time;
-}, 1000);
+document.addEventListener("DOMContentLoaded", function () {
+  // Timer function to present local date and time in header
+  const currentDay = document.querySelector(".currentDay");
+  setInterval(() => {
+    let time = dayjs().format("MM-DD-YYYY hh:mm:ss A");
+    currentDay.innerHTML = time;
+  }, 1000);
+});
+
+var APIKey = "34076cf95082839460dfb8d261ada036";
+var cityNameInput = document.getElementById("cityName");
+var city = cityNameInput.value;
+var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
+fetch(queryURL);
