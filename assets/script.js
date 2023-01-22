b0d6f089c9fbe6cfd834c0bf78ae32f046722cd1
Doomtower <jessehudak707@outlook.com>
2023-01-21 17:47:27 -0800

    upload for pull to laptop

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 var APIKey = "34076cf95082839460dfb8d261ada036";
+var city; 
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+// Timer function to present local date and time in header 
 const currentDay = document.querySelector(".currentDay");
 setInterval(() => {
   let time = dayjs().format("MM-DD-YYYY hh:mm:ss A");
