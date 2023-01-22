bd0f3e8bcb0557dbfcf5a0a4e6b73abc9e63d235
Doomtower <jessehudak707@outlook.com>
2023-01-22 14:50:40 -0800

    city search form working and displaying city& country name, weather, temp, wind and humidity

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key} - key for 5 day forcast
+var APIKey = "34076cf95082839460dfb8d261ada036";
+var queryURL = "https://api.openweathermap.org/data/2.5";
 
 document.addEventListener("DOMContentLoaded", function () {
   // Timer function to present local date and time in header
@@ -9,12 +11,41 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 1000);
 });
 
-var APIKey = "34076cf95082839460dfb8d261ada036";
-var cityNameInput = document.getElementById("cityName");
-var city = cityNameInput.value;
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=" +
-  city +
-  "&appid=" +
-  APIKey;
-fetch(queryURL);
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  getCityName();
+});
+
+function getCityName() {
+  var cityNameInput = document.getElementById("cityName");
+  var city = cityNameInput.value;
+  queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then((data) => {
+      // Display current searched weather data
+      var cityState = document.querySelector(".city-state");
+      cityState.innerHTML = data.name + ", " + data.sys.country;
+      var currentWeather = data.weather[0].main;
+      var kelvinTemp = data.main.temp;
+      var currentTempF = ((kelvinTemp - 273.15) * 9) / 5 + 32;
+      var currentHumidity = data.main.humidity;
+      var currentWind = data.wind.speed;
+      var currentIcon = data.weather[0].icon;
+      var currentDisplay = document.querySelector(".currentDisplay");
+      cityNameInput.value = "";
+      currentDisplay.innerHTML = `<p>City: ${data.name}, ${data.sys.country}</p>
+      <p>Weather: ${currentWeather}</p>
+      <p>Temperature: ${currentTempF.toFixed(2)}F</p>
+      <p>Humidity: ${currentHumidity}</p>
+      <p>Wind Speed: ${currentWind}</p>
+      <img src="https://openweathermap.org/img/w/${currentIcon}.png">`;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
